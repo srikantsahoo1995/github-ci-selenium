ec568bfaad8c4edc0279fa27817f60c61f449450
srikantsahoo1995 <srikantsahoo1995@gmail.com>
2025-04-24 21:39:00 +0530

    first commit

---
+++ src/test/java/org/example/GitCISelenium.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.v85.systeminfo.SystemInfo;
+import org.testng.annotations.Test;
+
+public class GitCISelenium {
+
+    @Test
+    public void testBody() throws InterruptedException {
+
+        WebDriver driver =  new ChromeDriver();
+        driver.get("https://www.google.com/");
+        Thread.sleep(5000);
+
+        System.out.println(driver.getTitle());
+
+        driver.quit();
+
+
+    }
+}
